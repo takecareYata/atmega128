e903b79461a5ebbf917587e71243eccc7508c3a7
takecareYata <ttrisq@gamil.com>
2026-06-10 16:24:07 +0900

    6월 10일 공부내용

---
+++ ai2/ATMEGA128A/01_LED_CONTROL/LED.c
@@ -0,0 +1,58 @@
+﻿/*
+ * led.c
+ *
+ * Created: 2026-06-10 오후 2:50:22
+ *  Author: yata
+ */ 
+
+#include "led.h"
+
+void init_led(void)
+{
+	DDRA = 0xff;
+	led_all_off();
+}
+
+void led_all_on(void)
+{
+	PORTA = 0xff;
+}
+
+void led_all_off(void)
+{
+	PORTA = 0x00;
+}
+
+void led_right_on(void)
+{
+	PORTA = 0x0f;
+}
+
+void led_left_on(void)
+{
+	PORTA = 0xf0;
+}
+
+void led_odd_on(void)
+{
+	if(PORTA == 0xAA)
+	{
+		PORTA >>= 1;
+	}
+	else
+	{
+		PORTA = 0x55;
+	}
+}
+
+void led_even_on(void)
+{
+	if(PORTA == 0x55)
+	{
+		PORTA <<= 1;
+	}
+	else
+	{
+		PORTA = 0xAA;
+	}
+}
