bfb27ed7bfd1a2cbae91b0d61ad83cadd238da2b
takecareYata <ttrisq@gamil.com>
2026-06-12 08:55:51 +0900

    6월 11일 숙제

---
+++ homework/Homework_0611_1/Homework_0611_1/led.h
@@ -0,0 +1,31 @@
+﻿/*
+ * led.h
+ *
+ * Created: 2026-06-10 오후 2:50:57
+ *  Author: yata
+ */ 
+#ifndef LED_H_
+#define LED_H_
+
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+extern void init_led(void);
+extern void led_all_on(void);
+extern void led_all_off(void);
+extern void led_right_on(void);
+extern void led_left_on(void);
+extern void led_odd_on(void);
+extern void led_even_on(void);
+extern void led_shift_right_on(void);
+extern void led_shift_left_on(void);
+extern void led_shift_right_keep_on(void);
+extern void led_shift_left_keep_on(void);
+extern void led_flower_on(void);
+extern void led_flower_off(void);
+
+extern int flower_on_flag;
+extern int flower_off_flag;
+
+#endif /* LED_H_ */
