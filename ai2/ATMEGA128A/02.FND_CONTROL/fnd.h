4a9ca69a276436e65a3bb0b752028369ab50852e
takecareYata <ttrisq@gamil.com>
2026-06-12 16:26:08 +0900

    6월 12일 수업내용 fnd 제어

---
+++ ai2/ATMEGA128A/02.FND_CONTROL/fnd.h
@@ -0,0 +1,31 @@
+﻿/*
+ * fnd.h
+ *
+ * Created: 2026-06-12 오전 10:45:30
+ *  Author: yata
+ */ 
+#ifndef FND_H_
+#define FND_H_
+
+
+#define F_CPU	16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define COMMON_ANODE
+
+#define FND_DATA_DDR	DDRC
+#define FND_DATA_PORT	PORTC
+
+#define FND_DIGIT_DDR	DDRB
+#define FND_DIGIT_PORT	PORTB
+#define FND_DIGIT_D1	4
+#define FND_DIGIT_D2	5
+#define FND_DIGIT_D3	6
+#define FND_DIGIT_D4	7
+
+
+
+
+
+#endif /* FND_H_ */
